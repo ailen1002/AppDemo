003b3f70718fb1bcc7aa9b35eaa067513eff98d1
Andrew <ailen1002@163.com>
2024-02-23 11:43:54 +0800

    first commit

---
+++ AppDemo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AppDemo.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AppDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AppDemo.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
