5703fb30c328a5ed6a8e0fb26a4517e8cc5019fe
Andrew <ailen1002@163.com>
2024-03-07 15:32:01 +0800

    新建Home,Settings页面。同时使用Carousel完成切换页面功能

--- AppDemo/Views/MainWindow.axaml.cs
+++ AppDemo/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace AppDemo.Views;
 
@@ -7,5 +11,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DrawerList.PointerReleased += DrawerSelectionChanged;
+        DrawerList.KeyUp += DrawerList_KeyUp;
+    }
+    private void DrawerList_KeyUp(object? sender, KeyEventArgs e) {
+        if (e.Key is Key.Space or Key.Enter)
+            DrawerSelectionChanged(sender, null);
+    }
+    public void DrawerSelectionChanged(object? sender, RoutedEventArgs? args) {
+        if (sender is not ListBox listBox)
+            return;
+
+        if (listBox is { IsFocused: false, IsKeyboardFocusWithin: false })
+            return;
+        try {
+            PageCarousel.SelectedIndex = listBox.SelectedIndex;
+            MainScroller.Offset = Vector.Zero;
+            MainScroller.VerticalScrollBarVisibility =
+                listBox.SelectedIndex == 5 ? ScrollBarVisibility.Disabled : ScrollBarVisibility.Auto;
+        }
+        catch {
+            // ignored
+        }
+        LeftDrawer.OptionalCloseLeftDrawer();
     }
 }
